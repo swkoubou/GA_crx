1a1aabf98ac6c89cb1a1a0e018500f9936175e22
NISHI3 <ryonishi3@gmail.com>
2016-08-13 06:43:51 +0900

    なんか作った変なの

--- hoge.js
+++
@@ -1,4 +0,0 @@
-function ToMD5() {
-    var seed = document.getElementById('input1').value;
-    document.getElementById('input3').value = CybozuLabs.MD5.calc(seed);
-}
--- hogehoge.js
+++
@@ -1,4 +0,0 @@
-function hogehoge(){
-  var str=  document.getElementById('input3').value;
-  document.getElementById('input3').value=str.slice(0,8);
-}
