45860dd11b4787f6e6a846830a8cf66614b0958d
ryuPechi <programeraiza@hotmail.co.jp>
2016-08-19 11:36:12 +0900

    Delete button2.js

--- button2.js
+++
@@ -1,6 +0,0 @@
-window.onload = function() {
-  document.getElementById(hogehoge).addEventListener("click", function hogehoge() {
-    var str=  document.getElementById('input3').value;
-    document.getElementById('input3').value=str.slice(0,16);
-  })
-};
